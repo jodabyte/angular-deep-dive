3534db9f214f7c33ecafb8180f33a1cd9460c585
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-06-30 14:49:47 +0000

    create a reusable component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Header } from './core/header/header';
+import { DashboardItem } from './features/dashboard/dashboard-item/dashboard-item';
 import { ServerStatus } from './features/dashboard/server-status/server-status';
 import { Tickets } from './features/dashboard/tickets/tickets';
 import { Traffic } from './features/dashboard/traffic/traffic';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, ServerStatus, Traffic, Tickets],
+  imports: [Header, ServerStatus, Traffic, Tickets, DashboardItem],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/features/dashboard/dashboard-item/dashboard-item.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  imports: [],
+  templateUrl: './dashboard-item.html',
+  styleUrl: './dashboard-item.css',
+})
+export class DashboardItem {
+  image = input.required<{
+    src: string;
+    alt: string;
+  }>();
+  title = input.required<string>();
+}
