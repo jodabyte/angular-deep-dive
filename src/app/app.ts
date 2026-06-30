311e53db93ba906a39d2ec97fc72e7d3c3569556
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-06-30 14:08:08 +0000

    splitt the content in app into multiple components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,42 +1,13 @@
 import { Component } from '@angular/core';
+import { Header } from './core/header/header';
+import { ServerStatus } from './features/dashboard/server-status/server-status';
+import { Tickets } from './features/dashboard/tickets/tickets';
+import { Traffic } from './features/dashboard/traffic/traffic';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header, ServerStatus, Traffic, Tickets],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
-}
+export class App {}
