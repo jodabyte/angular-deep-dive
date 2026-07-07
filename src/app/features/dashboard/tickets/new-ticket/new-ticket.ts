5b26dab5696f3a39d4cf1fba087978532200f553
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-07-07 17:56:33 +0000

    add an example for viewchild, contentchild and ElementRef

--- src/app/features/dashboard/tickets/new-ticket/new-ticket.ts
+++ src/app/features/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Button } from '../../../../shared/button/button';
 import { Control } from '../../../../shared/control/control';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [Button, Control],
+  imports: [Button, Control, FormsModule],
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.css',
 })
-export class NewTicket {}
+export class NewTicket {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+
+  onSubmit(title: string, ticketText: string) {
+    this.form?.nativeElement.reset();
+  }
+}
--- src/app/shared/control/control.ts
+++ src/app/shared/control/control.ts
@@ -1,4 +1,11 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,8 +15,18 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
+    '(click)': 'onClick()',
   },
 })
 export class Control {
   label = input.required<string>();
+  private el = inject(ElementRef);
+  @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.label);
+    console.log(this.el);
+    console.log(this.control);
+  }
 }
