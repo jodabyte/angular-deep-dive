893882503c507069fbb1bd069806adc67bce76a6
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-07-14 11:11:18 +0000

    add structural directive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, computed, inject } from '@angular/core';
+import { Auth } from './auth/auth';
 import { AuthComponent } from './auth/auth.component';
 import { AuthService } from './auth/auth.service';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 
 @Component({
   selector: 'app-root',
-  imports: [AuthComponent, LearningResourcesComponent],
+  imports: [AuthComponent, LearningResourcesComponent, Auth],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
