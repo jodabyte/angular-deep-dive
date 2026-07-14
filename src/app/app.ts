3d430dbbd76530b65d8a9a1803bea0318fa31ae9
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-07-14 14:07:35 +0000

    add a custom pipe to transform temperature

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
-import { DatePipe, DecimalPipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { TemperaturePipe } from './temperature-pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [DatePipe, DecimalPipe],
+  imports: [DatePipe, TemperaturePipe],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
