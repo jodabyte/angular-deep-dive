1656cbadddb9271a384d7b7a2f480e5a90c28af6
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-07-17 07:20:05 +0000

    init project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AvailablePlacesComponent } from './places/available-places/available-places.component';
+import { UserPlacesComponent } from './places/user-places/user-places.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AvailablePlacesComponent, UserPlacesComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
