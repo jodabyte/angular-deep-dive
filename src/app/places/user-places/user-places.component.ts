ccdef72ac32c2550df85ede3bc5583c16a8e60cc
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-07-18 15:03:05 +0000

    can delete favorite place

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,5 +1,6 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
+import { Place } from '../place.model';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
 import { PlacesService } from '../places.service';
@@ -28,6 +29,14 @@ export class UserPlacesComponent implements OnInit {
         this.isFetching.set(false);
       },
     });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
