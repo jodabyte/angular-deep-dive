586dc7273e05c3aa323f2b6e11730edae7e6d07e
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-07-18 14:48:18 +0000

    add global error handling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AvailablePlacesComponent } from './places/available-places/available-places.component';
 import { UserPlacesComponent } from './places/user-places/user-places.component';
+import { ErrorService } from './shared/error.service';
+import { ErrorModalComponent } from './shared/modal/error-modal/error-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [AvailablePlacesComponent, UserPlacesComponent],
+  imports: [AvailablePlacesComponent, UserPlacesComponent, ErrorModalComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  private errorService = inject(ErrorService);
+
+  error = this.errorService.error;
+}
