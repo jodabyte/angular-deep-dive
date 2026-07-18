ebef22012b871f2ad88857729d18d94f29ba8088
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-07-18 15:23:13 +0000

    add http interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,39 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpEventType,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { tap } from 'rxjs';
 import { routes } from './app.routes';
 
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  // const req = request.clone({
+  //   headers: request.headers.set('Test', 'Test'),
+  // });
+  console.log('[Outgoing Request]');
+  console.log(request);
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    }),
+  );
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([loggingInterceptor])),
+  ],
 };
