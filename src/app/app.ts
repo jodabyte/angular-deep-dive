a8fd3782f8b5d2e79164ff1a3b1a2f961ff5aac2
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-07-20 12:38:58 +0000

    init project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UsersComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
