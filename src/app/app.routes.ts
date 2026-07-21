a09aff74266d76d7c8b64ee63513dc2a1377e2f7
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-07-21 12:11:03 +0000

    add sorting as query parameter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { NotFound } from './not-found/not-found';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { routes as userRoutes } from './users/users.routes';
 
 export const routes: Routes = [
   {
@@ -13,21 +12,7 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'prefix',
-      },
-      {
-        path: 'tasks',
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-      },
-    ],
+    children: userRoutes,
   },
   {
     path: '**',
